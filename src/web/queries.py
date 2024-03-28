e914894a48cc382acf88eae8b4294ad23f6cb609
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-28 20:26:40 +0100

    Drop comment

--- src/web/queries.py
+++ src/web/queries.py
@@ -10,10 +10,6 @@ query = QueryType()
 
 @query.field("allIngredients")
 def resolve_all_ingredients(*_):
-    """
-    Positional params: see https://ariadnegraphql.org/docs/resolvers.html.
-    The `*_` is a python convention to ignore a list of positional params.
-    """
     return data.ingredients
 
 
